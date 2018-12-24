d806b09a3cf9cfdec1c6b1f4699e93063c7f99b0
robotcator <problemset@163.com>
2018-12-24 15:41:31 +0800

    first commit

--- install_anaconda3.sh
+++ install_anaconda3.sh
@@ -1,5 +1,4 @@
 curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 
-# yes for agreement, no for vs code
-yes no | bash Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b
 
